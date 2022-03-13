eb03321b0a445f3dd48c5cbd133fd093f52acef2
Sdoerty <roma-first@mail.ru>
2022-03-13 15:46:32 +0300

    display data from firebaseFirestore

--- lib/pages/travel_table.dart
+++ lib/pages/travel_table.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 class TravelTable extends StatefulWidget {
@@ -15,7 +17,6 @@ class _TravelTableState extends State<TravelTable>
   void initState() {
     super.initState();
     _tabController = TabController(length: 3, vsync: this);
-
   }
 
   @override
@@ -43,134 +44,140 @@ class _TravelTableState extends State<TravelTable>
                 ),
                 Flexible(
                   child: TabBarView(
-                children: <Widget>[
-                  // Мои путешествия
-                  SafeArea(
-                    // ListView позволяет включить скроллинг обектов внутри себя
-                    child: ListView.builder(
-                        padding: const EdgeInsets.all(8),
-                        itemCount: 3,
-                        itemBuilder: (BuildContext context, int index) {
-                          return SizedBox(
-                            height: 430,
-                            child: Center(
-                              child: Column(
-                                children: [
-                                  // Карточка путешествия
-                                  GestureDetector(
-                                    onTap: () => Navigator.pushNamed(context, '/bottom_navigation/travel_page'),
-                                    child: Container(
-                                      margin: const EdgeInsets.all(8.0),
-                                      child: Card(
-                                        shape: const RoundedRectangleBorder(
-                                            borderRadius: BorderRadius.all(
-                                                Radius.circular(10.0))),
-                                        child: Column(
-                                          crossAxisAlignment:
-                                          CrossAxisAlignment.stretch,
-                                          children: [
-                                            Stack(
-                                              children: [
-                                                const ClipRRect(
-                                                    borderRadius:
-                                                    BorderRadius.only(
-                                                      topLeft:
-                                                      Radius.circular(
-                                                          10.0),
-                                                      topRight:
-                                                      Radius.circular(
-                                                          10.0),
+                    children: <Widget>[
+                      // Мои путешествия
+                      SafeArea(
+                        // ListView позволяет включить скроллинг обектов внутри себя
+                        child: StreamBuilder(
+                          stream: FirebaseFirestore.instance
+                              .collection('travel')
+                              .snapshots(),
+                          builder: (BuildContext context,
+                              AsyncSnapshot<QuerySnapshot> snapshot) {
+                            if (!snapshot.hasData){
+                              return Center(
+                                  child: Text('Нет записей'));
+                            }else{
+                              return ListView.builder(
+                                  padding: const EdgeInsets.all(8),
+                                  itemCount: snapshot.data?.docs.length,
+                                  itemBuilder: (BuildContext context, int index) {
+                                    return SizedBox(
+                                      height: 430,
+                                      child: Dismissible(
+                                        key: Key(snapshot.data!.docs[index].id),
+                                        child: GestureDetector(
+                                          onTap: () => Navigator.pushNamed(
+                                              context,
+                                              '/bottom_navigation/travel_page'),
+                                          child: Container(
+                                            margin: const EdgeInsets.all(8.0),
+                                            child: Card(
+                                              shape: const RoundedRectangleBorder(
+                                                  borderRadius: BorderRadius.all(
+                                                      Radius.circular(10.0))),
+                                              child: Column(
+                                                crossAxisAlignment:
+                                                CrossAxisAlignment.stretch,
+                                                children: [
+                                                  Stack(
+                                                    children: [
+                                                      const ClipRRect(
+                                                          borderRadius:
+                                                          BorderRadius.only(
+                                                            topLeft:
+                                                            Radius.circular(
+                                                                10.0),
+                                                            topRight:
+                                                            Radius.circular(
+                                                                10.0),
+                                                          ),
+                                                          child: Image(
+                                                              image: AssetImage(
+                                                                  'assets/mountain.jpeg'))),
+                                                      Positioned(
+                                                          top: 7,
+                                                          right: 10,
+                                                          child: IconButton(
+                                                            onPressed: () {},
+                                                            icon: const Icon(
+                                                                Icons.share),
+                                                            color: Colors.white,
+                                                          ))
+                                                    ],
+                                                  ),
+                                                  ListTile(
+                                                    title: Text(
+                                                      snapshot.data!.docs[index]
+                                                          .get('travel_title'),
+                                                      style: TextStyle(
+                                                          fontSize: 18,
+                                                          fontWeight:
+                                                          FontWeight.bold),
                                                     ),
-                                                    child: Image(
-                                                        image: AssetImage(
-                                                            'assets/mountain.jpeg'))),
-                                                Positioned(
-                                                    top: 7,
-                                                    right: 10,
-                                                    child: IconButton(
-                                                      onPressed: () {},
-                                                      icon: const Icon(
-                                                          Icons.share),
-                                                      color: Colors.white,
-                                                    ))
-                                              ],
-                                            ),
-                                            const ListTile(
-                                              title: Text(
-                                                'Финляндия - Норвегия - Швеция - Эстония',
-                                                style: TextStyle(
-                                                    fontSize: 18,
-                                                    fontWeight:
-                                                    FontWeight.bold),
+                                                    subtitle: Text(snapshot
+                                                        .data!.docs[index]
+                                                        .get(
+                                                        'travel_description')),
+                                                  ),
+                                                  SizedBox(height: 15,),
+                                                  Row(
+                                                    mainAxisAlignment:
+                                                    MainAxisAlignment
+                                                        .spaceAround,
+                                                    children: [
+                                                      Column(
+                                                        children: [
+                                                          Text(
+                                                              '${snapshot.data!.docs[index].get('travel_date').toDate()}')
+                                                        ],
+                                                      ),
+                                                      Column(
+                                                        children: [
+                                                          Row(
+                                                            children: const [
+                                                              Icon(
+                                                                  Icons.thumb_up),
+                                                              Text('125')
+                                                            ],
+                                                          )
+                                                        ],
+                                                      ),
+                                                    ],
+                                                  ),
+                                                ],
                                               ),
-                                              subtitle: Text(
-                                                  'Путешествие по Скандинавским странам с конца 2019 года до конца зимы 2020 года'),
                                             ),
-                                            Container(
-                                              child: const Padding(
-                                                  padding: EdgeInsets.only(
-                                                      top: 10)),
-                                            ),
-                                            Row(
-                                              mainAxisAlignment:
-                                              MainAxisAlignment
-                                                  .spaceAround,
-                                              children: [
-                                                Column(
-                                                  children: const [
-                                                    Text('20.03.2020')
-                                                  ],
-                                                ),
-                                                Column(
-                                                  children: [
-                                                    Row(
-                                                      children: const [
-                                                        Icon(Icons.thumb_up),
-                                                        Text('125')
-                                                      ],
-                                                    )
-                                                  ],
-                                                ),
-                                              ],
-                                            ),
-                                            Container(
-                                              child: const Padding(
-                                                  padding: EdgeInsets.only(
-                                                      bottom: 10)),
-                                            )
-                                          ],
+                                          ),
                                         ),
                                       ),
-                                    ),
-
-                                  )
-                                ],
-                              ),
+                                    );
+                                  });
+                            }
+                          },
+                        ),
+                        /*child: */
+                      ),
+                      // Незавершенные путешествия
+                      SafeArea(
+                        child: Column(
+                          children: [
+                            const Divider(),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: const [
+                                Padding(
+                                    padding: EdgeInsets.only(
+                                        left: 0, top: 25, right: 0, bottom: 0)),
+                                Text('Незавершенные путешествия',
+                                    style: TextStyle(fontSize: 18))
+                              ],
                             ),
-                          );
-                        }),
-                    /*child: */
-                  ),
-                  // Незавершенные путешествия
-                  SafeArea(
-                    child: Column(
-                      children: [
-                        const Divider(),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: const [
-                            Padding(
-                                padding: EdgeInsets.only(
-                                    left: 0, top: 25, right: 0, bottom: 0)),
-                            Text('Незавершенные путешествия',
-                                style: TextStyle(fontSize: 18))
+                            const Divider(),
                           ],
                         ),
-                        const Divider(),
-                      ],
-                    ),
-                  ),
-                ],
+                      ),
+                    ],
                   ),
                 )
               ],
