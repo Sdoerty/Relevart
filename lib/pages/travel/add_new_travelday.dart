780409776d2192e0122cf5b4ffd45f318682ee82
Sdoerty <roma-first@mail.ru>
2022-04-14 19:25:10 +0300

    change some fields

--- lib/pages/travel/add_new_travelday.dart
+++ lib/pages/travel/add_new_travelday.dart
@@ -11,121 +11,155 @@ class AddNewTravelday extends StatelessWidget {
         centerTitle: true,
         title: Text("Новая запись путешествия"),
       ),
-      body: SafeArea(
-          child: Padding(
-            padding: EdgeInsets.only(left: 15, top: 20, right: 15, bottom: 0),
-            child: Column(
-              children: [
-                SizedBox(height: 10),
-                TextField(
+      body: Container(
+        color: Color.fromRGBO(243, 243, 243, 0.9),
+        child: Padding(
+          padding: EdgeInsets.only(left: 15, top: 20, right: 15, bottom: 0),
+          child: Column(
+            children: [
+              SizedBox(height: 10),
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                  boxShadow: [
+                    new BoxShadow(
+                      color: Colors.black54,
+                      offset: new Offset(1.0, 1.0),
+                      blurRadius: 3.0,
+                    )
+                  ],
+                ),
+                child: TextField(
                   keyboardType: TextInputType.multiline,
                   minLines: 5,
                   maxLines: 5,
                   decoration: InputDecoration(
                       border: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(30))),
+                          borderRadius: BorderRadius.all(Radius.circular(10))),
                       labelText: "Описание дня",
                       floatingLabelBehavior: FloatingLabelBehavior.always,
                       floatingLabelAlignment: FloatingLabelAlignment.center,
                       hintText: 'опишите всё что происходило в этот день...'),
                 ),
-                SizedBox(height: 15),
-                SizedBox(
+              ),
+              SizedBox(height: 15),
+              SizedBox(
+                  child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                      boxShadow: [
+                        new BoxShadow(
+                          color: Colors.black54,
+                          offset: new Offset(1.0, 1.0),
+                          blurRadius: 3.0,
+                        )
+                      ],
+                    ),
+                    padding: EdgeInsets.symmetric(vertical: 20, horizontal: 50),
                     child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       children: [
-                        Column(
-                          children: [
-                            Container(
-                              child: Text(
-                                "01.01.2000",
-                                style: TextStyle(
-                                    fontSize: 24,
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.blueGrey),
-                              ),
-                            ),
-                            Container(
-                                child:
-                                OutlinedButton.icon(
-                                  onPressed: () {},
-                                  icon: Icon(
-                                    Icons.date_range_outlined,
-                                    color: Colors.blueGrey,
-                                  ),
-                                  label: Text(
-                                    'Выбрать дату событий',
-                                    style: TextStyle(color: Colors.blueGrey),
-                                  ),
-                                )),
-                          ],
+                        Text(
+                          "01.01.2000",
+                          style: TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.blueGrey),
                         ),
-                        Divider(),
-                        Column(
-                          children: [
-                            Container(
-                                margin: EdgeInsets.symmetric(horizontal: 50),
-                                decoration: BoxDecoration(
-                                    border: Border.all(color: Colors.grey)
-                                ),
-                                height: 100,
-                                child: Center(child: Text('Нет обложки'))),
-                            Container(
-                              alignment: Alignment.center,
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: <Widget>[
-                                  OutlinedButton.icon(
-                                    onPressed: () {},
-                                    icon: Icon(
-                                      Icons.photo_album,
-                                      color: Colors.blueGrey,
-                                    ),
-                                    label: Text(
-                                      'Добавить фото',
-                                      style: TextStyle(color: Colors.blueGrey),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ],
+                        OutlinedButton.icon(
+                          onPressed: () {},
+                          icon: Icon(
+                            Icons.date_range_outlined,
+                            color: Colors.blueGrey,
+                          ),
+                          label: Text(
+                            'Выбрать дату событий',
+                            style: TextStyle(color: Colors.blueGrey),
+                          ),
                         ),
-                        Divider(),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 15),
+                  Container(
+                    padding: EdgeInsets.symmetric(vertical: 20),
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                        boxShadow: [
+                          new BoxShadow(
+                            color: Colors.black54,
+                            offset: new Offset(1.0, 1.0),
+                            blurRadius: 3.0,
+                          )
+                        ]),
+                    child: Column(
+                      children: [
                         Container(
                             margin: EdgeInsets.symmetric(horizontal: 50),
                             decoration: BoxDecoration(
-                                border: Border.all(color: Colors.grey)
-                            ),
+                                border: Border.all(color: Colors.grey)),
                             height: 100,
-                            child: Center(child: Text('отметки на карте'))),
+                            child: Center(child: Text('Нет обложки'))),
+                        Container(
+                          alignment: Alignment.center,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              OutlinedButton.icon(
+                                onPressed: () {},
+                                icon: Icon(
+                                  Icons.photo_album,
+                                  color: Colors.blueGrey,
+                                ),
+                                label: Text(
+                                  'Добавить фото',
+                                  style: TextStyle(color: Colors.blueGrey),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
                       ],
-                    )),
-
-                Divider(
-                  height: 1,
-                ),
-                SizedBox(height: 15),
-                Divider(
-                  height: 1,
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 20),
-                  child: SizedBox(
-                    width: double.infinity,
-                    child: ElevatedButton(
-                        onPressed: (){},
-                        child: Text('Добавить запись'),
-                        style: ElevatedButton.styleFrom(
-                            primary: Colors.blueGrey,
-                            padding: EdgeInsets.symmetric(vertical: 10),
-                            textStyle: TextStyle(
-                                fontSize: 20, fontWeight: FontWeight.bold))),
+                    ),
                   ),
+                  SizedBox(height: 15),
+                  Container(
+                      margin: EdgeInsets.symmetric(horizontal: 50),
+                      decoration:
+                          BoxDecoration(border: Border.all(color: Colors.grey)),
+                      height: 100,
+                      child: Center(child: Text('отметки на карте'))),
+                ],
+              )),
+              Divider(
+                height: 1,
+              ),
+              SizedBox(height: 15),
+              Divider(
+                height: 1,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 20),
+                child: SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                      onPressed: () {},
+                      child: Text('Добавить запись'),
+                      style: ElevatedButton.styleFrom(
+                          primary: Colors.blueGrey,
+                          padding: EdgeInsets.symmetric(vertical: 10),
+                          textStyle: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold))),
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
+        ),
       ),
     );
   }
